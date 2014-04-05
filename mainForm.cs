99b54f78d90f6e61a97f5b8b139e6a01f6a5d067
Max Azoury <xxcooloppoxx@gmail.com>
2014-04-04 22:24:29 -0400

    Remove unused files and fix filenames (instead of originalfile.ext.opus, it's originalfile.opus now!)

--- mainForm.cs
+++ mainForm.cs
@@ -1,61 +1,62 @@
 ﻿using System;
 using System.ComponentModel;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Opus_Encoder_GUI
 {
-    public partial class MainForm : Form
-    {
-        readonly ConvertingDialog _objConvertingDialog = new ConvertingDialog();
+	public partial class MainForm : Form
+	{
+		readonly ConvertingDialog _objConvertingDialog = new ConvertingDialog();
 
-        public MainForm()
-        {
-            InitializeComponent();
-        }
+		public MainForm()
+		{
+			InitializeComponent();
+		}
 
-        void button1_Click(object sender, EventArgs e)
-        {
-            openFileDialog1.ShowDialog();
-        }
+		void button1_Click(object sender, EventArgs e)
+		{
+			openFileDialog1.ShowDialog();
+		}
 
-        void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-            textBox1.Text = openFileDialog1.FileName;
-        }
+		void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+		{
+			textBox1.Text = openFileDialog1.FileName;
+		}
 
-        void convertButton_Click(object sender, EventArgs e)
-        {
-            StartConverting();
-        }
+		void convertButton_Click(object sender, EventArgs e)
+		{
+			StartConverting();
+		}
 
-        void StartConverting()
-        {
-            const string space = " ";
-            const string doubleQuote = "\"";
-            opusencProcess.StartInfo.Arguments = "--bitrate" + space + bitrateAdjuster.Value + space + doubleQuote + openFileDialog1.FileName + doubleQuote + space + doubleQuote + openFileDialog1.FileName + ".opus" + doubleQuote;
-            opusencProcess.Start();
-            _objConvertingDialog.ShowDialog(this);
-        }
+		void StartConverting()
+		{
+			const string space = " ";
+			const string doubleQuote = "\"";
+			opusencProcess.StartInfo.Arguments = "--bitrate" + space + bitrateAdjuster.Value + space + doubleQuote + openFileDialog1.FileName + doubleQuote + space + doubleQuote + Path.GetDirectoryName(openFileDialog1.FileName) + "\\" + Path.GetFileNameWithoutExtension(openFileDialog1.FileName) + ".opus" + doubleQuote;
+			opusencProcess.Start();
+			_objConvertingDialog.ShowDialog(this);
+		}
 
-        void process1_Exited(object sender, EventArgs e)
-        {
-            _objConvertingDialog.Hide();
-        }
+		void process1_Exited(object sender, EventArgs e)
+		{
+			_objConvertingDialog.Hide();
+		}
 
-        void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            if (textBox1.Text.Length > 0 && convertButton.Enabled == false)
-            {
-                convertButton.Enabled = true;
-            }
-            else if (textBox1.Text.Length <= 0 && convertButton.Enabled)
-            {
-                convertButton.Enabled = false;
-            }
-        }
+		void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			if (textBox1.Text.Length > 0 && convertButton.Enabled == false)
+			{
+				convertButton.Enabled = true;
+			}
+			else if (textBox1.Text.Length <= 0 && convertButton.Enabled)
+			{
+				convertButton.Enabled = false;
+			}
+		}
 
-        void mainForm_Load(object sender, EventArgs e)
-        {
-        }
-    }
+		void mainForm_Load(object sender, EventArgs e)
+		{
+		}
+	}
 }
